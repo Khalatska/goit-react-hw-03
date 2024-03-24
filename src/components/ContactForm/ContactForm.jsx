33a8b7d9102d4edfbb76cc9bfe9ddd083c5b3d91
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-24 17:32:11 +0100

    deleted folder

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,24 @@
-import { FORM_INITIAL_VALUES } from "../../utils/constants";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { MAX_CHAR_NAME_VALIDATION } from "../../utils/constants";
-import { MIN_CHAR_NAME_VALIDATION } from "../../utils/constants";
 import css from "./ContactForm.module.css";
+
+const FORM_INITIAL_VALUES = {
+  name: "",
+  number: "",
+};
+
+const MAX_CHAR_NAME_VALIDATION = 50;
+const MIN_CHAR_NAME_VALIDATION = 3;
 
 const FeedbackSchema = Yup.object().shape({
   name: Yup.string()
+    .min(
+      MIN_CHAR_NAME_VALIDATION,
+      `Your user name must be at least ${MIN_CHAR_NAME_VALIDATION} characters!`
+    )
     .max(
       MAX_CHAR_NAME_VALIDATION,
       `Your user name must be less than ${MAX_CHAR_NAME_VALIDATION} characters!`
-    )
-    .min(
-      MIN_CHAR_NAME_VALIDATION,
-      `Your user name must be less than ${MIN_CHAR_NAME_VALIDATION} characters!`
     )
     .required("Name is required!"),
   number: Yup.string().required("Number is required!"),
